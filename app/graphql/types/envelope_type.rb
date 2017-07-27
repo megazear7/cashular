9cc0debbac7728df9846884e3a08e40da6a04a60
alexlockhart7 <alex.lockhart@icfolson.com>
2017-07-27 14:29:33 -0400

    Made graphql interface awesome

--- app/graphql/types/envelope_type.rb
+++ app/graphql/types/envelope_type.rb
@@ -1,24 +1,71 @@
 require_relative "./transaction_type"
 require_relative "./user_type"
 
+def get_transactions transactions, args, ctx
+  if not ctx[:deleted].nil?
+    transactions.where!(deleted: ctx[:deleted])
+  else
+    transactions.where!(deleted: false)
+  end
+
+  if not ctx[:from].nil?
+    transactions.where!("post_date >= ?", ctx[:from])
+  end
+
+  if not ctx[:to].nil?
+    transactions.where!("post_date <= ?", ctx[:to])
+  end
+
+  if not ctx[:organized].nil? and ctx[:organized]
+    transactions = transactions.where.not(envelope_id: nil)
+  elsif not ctx[:organized].nil? and not ctx[:organized]
+    transactions.where!(envelope_id: nil)
+  end
+
+  return transactions
+end
+
 EnvelopeType = GraphQL::ObjectType.define do
   name "Envelope"
   field :id, !types.ID
   field :title, types.String
-  field :total, types.Float
-  field :transactions, types[TransactionType]
+  field :transactions do
+    type types[TransactionType]
+    argument :from, types.String
+    argument :to, types.String
+    argument :deleted, types.String
+    resolve -> (obj, args, ctx) {
+      return get_transactions(obj.transactions, args, ctx).order('post_date DESC')
+    }
+  end
   field :user do
     type UserType
     resolve -> (obj, args, ctx) {
       obj.user
     }
   end
-  field :all_time do
+  field :net do
     type types.Float
     argument :from, types.String
     argument :to, types.String
     resolve -> (obj, args, ctx) {
-      obj.sum(args[:from], args[:to])
+      return get_transactions(obj.transactions, args, ctx).sum(:amount)
+    }
+  end
+  field :loss do
+    type types.Float
+    argument :from, types.String
+    argument :to, types.String
+    resolve -> (obj, args, ctx) {
+      return get_transactions(obj.transactions, args, ctx).where("amount < ?", 0).sum(:amount).abs()
+    }
+  end
+  field :gain do
+    type types.Float
+    argument :from, types.String
+    argument :to, types.String
+    resolve -> (obj, args, ctx) {
+      return get_transactions(obj.transactions, args, ctx).where("amount > ?", 0).sum(:amount)
     }
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,14 +6,60 @@ QueryType = GraphQL::ObjectType.define do
   field :envelope do
     type EnvelopeType
     argument :id, !types.ID
+    argument :from, types.String
+    argument :to, types.String
+    argument :organized, types.Boolean
+    argument :deleted, types.Boolean
     description "Find a Envelope by ID"
-    resolve ->(obj, args, ctx) { Envelope.find(args["id"]) }
+    resolve ->(obj, args, ctx) {
+      if args.key? :from
+        ctx[:from] = args[:from]
+        puts ctx[:from]
+      end
+
+      if args.key? :to
+        ctx[:to] = args[:to]
+      end
+
+      if args.key? :organized
+        ctx[:organized] = args[:organized]
+      end
+
+      if args.key? :deleted
+        ctx[:deleted] = args[:deleted]
+      end
+
+      Envelope.find(args["id"])
+    }
   end
 
   field :user do
     type UserType
     argument :id, !types.ID
+    argument :from, types.String
+    argument :to, types.String
+    argument :organized, types.Boolean
+    argument :deleted, types.Boolean
     description "Find a User by ID"
-    resolve ->(obj, args, ctx) { User.find(args["id"]) }
+    resolve ->(obj, args, ctx) {
+      if args.key? :from
+        ctx[:from] = args[:from]
+        puts ctx[:from]
+      end
+
+      if args.key? :to
+        ctx[:to] = args[:to]
+      end
+
+      if args.key? :organized
+        ctx[:organized] = args[:organized]
+      end
+
+      if args.key? :deleted
+        ctx[:deleted] = args[:deleted]
+      end
+
+      User.find(args["id"])
+    }
   end
 end
